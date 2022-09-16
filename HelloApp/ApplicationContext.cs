a3d980479092fc3199d14e472756837f1ff8eafc
opilane <opilane@HP-CZC6297B9M>
2022-09-16 10:33:42 +0300

    Add project files.

---
+++ HelloApp/ApplicationContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloApp
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class ApplicationContext : DbContext
+    {
+
+        //public DbSet<User> Users { get; set; } = null!;
+        public DbSet<User> Users => Set<User>();
+
+        public ApplicationContext() => Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=helloapp.db");
+        }
+
+    }
+}
---
+++ HelloApp/Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HelloApp;
+using (ApplicationContext db = new ApplicationContext())
+{
+    bool isCreated = db.Database.EnsureCreated();
+    if (isCreated) Console.WriteLine("Data Base is created");
+    else Console.WriteLine("Data base was already created");
+}
+using (helloappContext db = new helloappContext())
+{
+    // создаем два объекта User
+    //User tom = new User { Name = "Tom", Age = 33 };
+    //User alice = new User { Name = "Alice", Age = 26 };
+
+    // добавляем их в бд
+    //db.Users.Add(tom);
+    //db.Users.Add(alice);
+    //db.SaveChanges();
+    //Console.WriteLine("object are saved");
+
+    // получаем объекты из бд и выводим на консоль
+    var users = db.Users.ToList();
+    Console.WriteLine("objects:");
+    foreach (User u in users)
+    {
+        Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+    }
+}
+
+
