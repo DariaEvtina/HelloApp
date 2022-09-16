b10a96d6d5d2e73a0f2f05a06a8ea27d6696ba7d
mangle12369zzaa@gmail.com <mangle12369zzaa@gmail.com>
2022-09-16 11:32:31 +0300

    ghrdhsgde

--- HelloApp/ApplicationContext.cs
+++ HelloApp/ApplicationContext.cs
@@ -10,15 +10,11 @@ namespace HelloApp
 
     public class ApplicationContext : DbContext
     {
-
-        //public DbSet<User> Users { get; set; } = null!;
-        public DbSet<User> Users => Set<User>();
-
-        public ApplicationContext() => Database.EnsureCreated();
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        public DbSet<User> Users { get; set; } = null!;
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+                : base(options)
         {
-            optionsBuilder.UseSqlite("Data Source=helloapp.db");
+            Database.EnsureCreated();
         }
 
     }
--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using HelloApp;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 //https://metanit.com/sharp/efcore/2.16.php
-using (ApplicationContext db = new ApplicationContext())
+/*using (ApplicationContext db = new ApplicationContext())
 {
     //await db.Database.EnsureDeletedAsync();
     bool isCreated = db.Database.EnsureCreated();
@@ -11,7 +13,7 @@ using (ApplicationContext db = new ApplicationContext())
     if (isAvalaible) Console.WriteLine("Data base is avalible");
     else Console.WriteLine("data base is not avalible");
 }
-/*using (helloappContext db = new helloappContext())
+using (helloappContext db = new helloappContext())
 {
     // создаем два объекта User
     //User tom = new User { Name = "Tom", Age = 33 };
@@ -30,7 +32,7 @@ using (ApplicationContext db = new ApplicationContext())
     {
         Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
     }
-}*/
+}
 // Добавление
 using (helloappContext db = new helloappContext())
 {
@@ -83,5 +85,24 @@ using (helloappContext db = new helloappContext())
     {
         Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
     }
+}*/
+var builder = new ConfigurationBuilder();
+// установка пути к текущему каталогу
+builder.SetBasePath(Directory.GetCurrentDirectory());
+// получаем конфигурацию из файла appsettings.json
+builder.AddJsonFile("appsetings.json");
+// создаем конфигурацию
+var config = builder.Build();
+// получаем строку подключения
+string connectionString = config.GetConnectionString("DefaultConnection");
+
+var optionsBuilder = new DbContextOptionsBuilder<ApplicationContext>();
+var options = optionsBuilder.UseSqlite(connectionString).Options;
+
+using (ApplicationContext db = new ApplicationContext(options))
+{
+    var users = db.Users.ToList();
+    foreach (User user in users)
+        Console.WriteLine($"{user.Id}.{user.Name} - {user.Age}");
 }
 
