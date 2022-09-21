0d63b64f5692bb4d0631e77c147b3bbb3d7c4481
mangle12369zzaa@gmail.com <mangle12369zzaa@gmail.com>
2022-09-21 11:14:30 +0300

    n b,,

--- HelloApp/ApplicationContext.cs
+++ HelloApp/ApplicationContext.cs
@@ -11,9 +11,15 @@ namespace HelloApp
     {
         public DbSet<User> Users { get; set; } = null!;
 
+        public ApplicationContext()
+        {
+            Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=helloappdb;Trusted_Connection=True;");
+            optionsBuilder.UseMySql("server=localhost;user=root;password=123456789;database=usersdb;",
+                new MySqlServerVersion(new Version(8, 0, 25)));
         }
     }
 }
--- HelloApp/Program.cs
+++ HelloApp/Program.cs
@@ -5,24 +5,21 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
+// добавление данных
 using (ApplicationContext db = new ApplicationContext())
 {
-    db.Database.EnsureDeleted();
-    db.Database.EnsureCreated();
-    // создаем два объекта User
+    
     User user1 = new User { Name = "Tom", Age = 33 };
     User user2 = new User { Name = "Alice", Age = 26 };
 
-    // добавляем их в бд
     db.Users.AddRange(user1, user2);
     db.SaveChanges();
 }
 // получение данных
 using (ApplicationContext db = new ApplicationContext())
 {
-    // получаем объекты из бд и выводим на консоль
     var users = db.Users.ToList();
-    Console.WriteLine("Users list:");
+    Console.WriteLine("Objects list:");
     foreach (User u in users)
     {
         Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
