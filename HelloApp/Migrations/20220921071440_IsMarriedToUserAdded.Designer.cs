8d82abef067a2ec66c97a0a67a010723fca0256c
mangle12369zzaa@gmail.com <mangle12369zzaa@gmail.com>
2022-09-21 10:14:52 +0300

    gfd

--- HelloApp/Migrations/20220921071440_IsMarriedToUserAdded.Designer.cs
+++ HelloApp/Migrations/20220921071440_IsMarriedToUserAdded.Designer.cs
@@ -2,6 +2,7 @@
 using HelloApp;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HelloApp.Migrations
 {
     [DbContext(typeof(ApplicationContext))]
-    partial class ApplicationContextModelSnapshot : ModelSnapshot
+    [Migration("20220921071440_IsMarriedToUserAdded")]
+    partial class IsMarriedToUserAdded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.9");
